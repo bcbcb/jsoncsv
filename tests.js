282f2537439ba8ac8af90ac2ae3f3d8a6b454df5
bcbcb <bcbcb@users.noreply.github.com>
2015-04-19 16:05:10 -0700

    Add tests with specs

---
+++ tests.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+var serverUrl = 'http://localhost:8080';
+var endpoint = '/api/convert';
+
+describe('JSON to CSV', function () {
+
+  describe('API', function () {
+ 
+    it('should have a RESTful endpoint', function (done) {
+      assert.equal(false, true);
+    }); 
+
+    it('should accept post request with JSON', function (done) {
+      assert.equal(false, true);
+    });
+
+  });
+
+  describe('CSV formatting', function () {
+
+    it('should validate that JSON can be transformed into CSV', function (done) {
+      assert.equal(false, true);
+    }); 
+
+    it('should return CSV formatted string for valid JSON', function (done) {
+      assert.equal(false, true);
+    });
+
+  });
+
+});
