e98a397939bbb8b19c93fa8db5e315546e5f76b9
bcbcb <bcbcb@users.noreply.github.com>
2015-04-20 00:46:17 -0700

    Refactor/cleanup

--- jsontocsv.js
+++ jsontocsv.js
@@ -1,5 +1,8 @@
 var convert = function (json) {
+  var properties;
+  var row;
   var csv = '';
+  var delimiter = ',';
   var errorMessage = 'Invalid JSON';
 
   // Validate JSON
@@ -12,25 +15,23 @@ var convert = function (json) {
     }
   }
 
-  // Build first row of CSV
-  var properties = Object.keys(json[0]);
+  // Build first row
+  properties = Object.keys(json[0]);
   csv += properties.join(',');
 
-  // Add rows of values separated by commas
+  // Build the rest of the rows
   for (var i = 0; i < json.length; i++) {
-    // Start new row of data
-    csv += '\n';
+    row = '';
     for (var j = 0; j < properties.length; j++) {
+      if (row !== '') {
+        row += ',';
+      } 
       if (json[i][properties[j]] !== undefined) {
-        csv += json[i][properties[j]];
-      }
-      // Add comma except for last value of row
-      if (j + 1 < properties.length) {
-        csv += ',';
+        row += json[i][properties[j]];
       }
     }
+    csv += '\n' + row;
   }
-
   return csv;
 };
 
