72521e47b54ed2913187b145da9b01e6968c4069
bcbcb <bcbcb@users.noreply.github.com>
2015-04-19 23:30:27 -0700

    Add more validation, made all tests pass

--- jsontocsv.js
+++ jsontocsv.js
@@ -1,10 +1,18 @@
-
-var convert = function (json, cb) {
+var convert = function (json) {
   var csv = '';
   var errorMessage = 'Invalid JSON';
 
+  // Validate JSON
   if (!Array.isArray(json)) {
     return errorMessage;
+  }
+  if (json.length === 0) {
+    return errorMessage;
+  }
+  for (var k = 0; k < json.length; k++) {
+    if (typeof json[k] !== 'object') {
+      return errorMessage;
+    }
   }
 
   // Build first row of CSV
@@ -15,8 +23,13 @@ var convert = function (json, cb) {
     // Start new row of data
     csv += '\n';
     for (var j = 0; j < properties.length; j++) {
-      csv += json[i][properties[j]];
-      // Add comma unless it's the last column
+      // Error if properties don't match up
+      if (json[i][properties[j]] === undefined) {
+        return errorMessage;
+      } else {
+        csv += json[i][properties[j]];
+      }
+      // Add comma except for last value of row
       if (j + 1 < properties.length) {
         csv += ',';
       }
--- tests.js
+++ tests.js
@@ -7,158 +7,156 @@ var jsontocsv = require('./jsontocsv.js');
 var serverUrl = 'http://localhost:8080';
 var endpointUrl = serverUrl + '/api/convert';
 
-describe('JSON to CSV', function () {
 
-  describe('CSV formatting', function () {
+describe('validation', function () {
+  var errorMessage = "Invalid JSON";
 
-    describe('validation', function () {
-      var errorMessage = "Invalid JSON";
-
-      it('should error if input is not array', function (done) {
-        assert.equal(jsontocsv.convert(123), errorMessage);
-        assert.equal(jsontocsv.convert('asdf'), errorMessage);
-        assert.equal(jsontocsv.convert({}), errorMessage);
-        done();
-      });
-
-      it('should error if array is empty', function (done) {
-        assert.equal(jsontocsv.convert('[]', errorMessage));
-        done();
-      });
-
-      it('should error if the number of properties of each element are not the same', function (done) {
-        var invalid1 = [
-          {
-            "first":"George",
-            "last":"Washington"
-          },
-          {
-            "first":"Bob"
-          }
-        ];
-        assert.equal(jsontocsv.convert(invalid1), errorMessage);
-        done();
-      });
-      
-      it('should error if property names of each element are not the same', function (done) {
-        var invalid2 = [
-          {
-            "first":"George",
-            "last":"Washington"
-          },
-          {
-            "first":"Bob",
-            "gender":"male"
-          }
-        ];
-
-        assert.equal(jsontocsv.convert(invalid2), errorMessage);
-        done();
-      });
-
-    });
-
+  it('should error if input is not array', function (done) {
+    assert.equal(jsontocsv.convert(123), errorMessage);
+    assert.equal(jsontocsv.convert('asdf'), errorMessage);
+    assert.equal(jsontocsv.convert({}), errorMessage);
+    done();
   });
 
-  describe('converter', function () {
-
-    it('should return a CSV formatted string for valid JSON', function (done) {
-
-      var json1 = [
-        {
-          "first":"Brenard",
-          "last":"Cubacub",
-          "gender":"male"
-        }
-      ];
-      var csv1 = 'first,last,gender\nBrenard,Cubacub,male';
-      assert.equal(jsontocsv.convert(json1), csv1);
-
-      var json2 = [
-        {
-          "name": "Adam",
-          "lastName": "Smith",
-          "gender": "male",
-          "country": "Scotland"
-        },
-        {
-          "name": "George",
-          "lastName": "Washington",
-          "gender": "male",
-          "country": "USA"
-        },
-        {
-          "name": "Marie",
-          "lastName": "Curie",
-          "gender": "female",
-          "country": "France"
-        }
-      ];
-      var csv2 = 'name,lastName,gender,country' + 
-        '\nAdam,Smith,male,Scotland' +
-        '\nGeorge,Washington,male,USA' +
-        '\nMarie,Curie,female,France';
-      assert.equal(jsontocsv.convert(json2), csv2);
-      
-      done();
-    });
-
+  it('should error if array is empty', function (done) {
+    assert.equal(jsontocsv.convert([]), errorMessage);
+    done();
   });
 
+  it('should error if array contains non-objects', function (done) {
+    assert.equal(jsontocsv.convert([{},1]), errorMessage);
+    done();
+  });
 
-  describe('server', function () {
+  it('should error if the number of properties of each element are not the same', function (done) {
+    var invalid1 = [
+      {
+        "first":"George",
+        "last":"Washington"
+      },
+      {
+        "first":"Bob"
+      }
+    ];
+    assert.equal(jsontocsv.convert(invalid1), errorMessage);
+    done();
+  });
+  
+  it('should error if property names of each element are not the same', function (done) {
+    var invalid2 = [
+      {
+        "first":"George",
+        "last":"Washington"
+      },
+      {
+        "first":"Bob",
+        "gender":"male"
+      }
+    ];
+
+    assert.equal(jsontocsv.convert(invalid2), errorMessage);
+    done();
+  });
+
+});
+
+describe('converter', function () {
+
+  it('should return a CSV formatted string for valid JSON', function (done) {
+
+    var json1 = [
+      {
+        "first":"Brenard",
+        "last":"Cubacub",
+        "gender":"male"
+      }
+    ];
+    var csv1 = 'first,last,gender\nBrenard,Cubacub,male';
+    assert.equal(jsontocsv.convert(json1), csv1);
+
+    var json2 = [
+      {
+        "name": "Adam",
+        "lastName": "Smith",
+        "gender": "male",
+        "country": "Scotland"
+      },
+      {
+        "name": "George",
+        "lastName": "Washington",
+        "gender": "male",
+        "country": "USA"
+      },
+      {
+        "name": "Marie",
+        "lastName": "Curie",
+        "gender": "female",
+        "country": "France"
+      }
+    ];
+    var csv2 = 'name,lastName,gender,country' + 
+      '\nAdam,Smith,male,Scotland' +
+      '\nGeorge,Washington,male,USA' +
+      '\nMarie,Curie,female,France';
+    assert.equal(jsontocsv.convert(json2), csv2);
     
-    before(function (done) {
-      server.start();
-      done();
-    });
+    done();
+  });
 
-    it('should return 200 from JSON POST request', function (done) {
-      request.post(endpointUrl)
-        .set('Content-Type', 'application/json')
-        .send('{"test":"testing"}')
-        .end(function (err, res) {
-          assert.equal(200, res.statusCode);
-          done();
-        });
-    });
+});
 
-    it('should respond with CSV after posting valid JSON', function (done) {
-      var json2 = [
-        {
-          "name": "Adam",
-          "lastName": "Smith",
-          "gender": "male",
-          "country": "Scotland"
-        },
-        {
-          "name": "George",
-          "lastName": "Washington",
-          "gender": "male",
-          "country": "USA"
-        },
-        {
-          "name": "Marie",
-          "lastName": "Curie",
-          "gender": "female",
-          "country": "France"
-        }
-      ];
-      var csv2 = 'name,lastName,gender,country' + 
-        '\nAdam,Smith,male,Scotland' +
-        '\nGeorge,Washington,male,USA' +
-        '\nMarie,Curie,female,France';
 
-      request.post(endpointUrl)
-        .send(json2)
-        .set('Content-Type', 'application/json')
-        .end(function (err, res) {
-          assert.equal(csv2, res.text);
-          assert.equal(200, res.statusCode);
-          done();
-        });
+describe('server', function () {
+  
+  before(function (done) {
+    server.start();
+    done();
+  });
 
-    });
+  it('should return 200 from JSON POST request', function (done) {
+    request.post(endpointUrl)
+      .set('Content-Type', 'application/json')
+      .send('{"test":"testing"}')
+      .end(function (err, res) {
+        assert.equal(200, res.statusCode);
+        done();
+      });
+  });
+
+  it('should respond with CSV after posting valid JSON', function (done) {
+    var json2 = [
+      {
+        "name": "Adam",
+        "lastName": "Smith",
+        "gender": "male",
+        "country": "Scotland"
+      },
+      {
+        "name": "George",
+        "lastName": "Washington",
+        "gender": "male",
+        "country": "USA"
+      },
+      {
+        "name": "Marie",
+        "lastName": "Curie",
+        "gender": "female",
+        "country": "France"
+      }
+    ];
+    var csv2 = 'name,lastName,gender,country' + 
+      '\nAdam,Smith,male,Scotland' +
+      '\nGeorge,Washington,male,USA' +
+      '\nMarie,Curie,female,France';
+
+    request.post(endpointUrl)
+      .send(json2)
+      .set('Content-Type', 'application/json')
+      .end(function (err, res) {
+        assert.equal(csv2, res.text);
+        assert.equal(200, res.statusCode);
+        done();
+      });
 
   });
 
